b69ab61e270bd890c7254efe7a7fb19fa818f1ac
pradeep p <ppradeep2008@gmail.com>
2020-11-08 22:20:06 +0530

    chag

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
 app.post("/users", (req, res) => {
